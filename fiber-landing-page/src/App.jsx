18dffdd6ba82f854d0c2f8c67f012825e4722b55
gouribhise <gouri.a.bhise@gmail.com>
2024-03-06 18:34:28 +0530

    added simple footer

--- fiber-landing-page/src/App.jsx
+++ fiber-landing-page/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
  
  
-import { Navbar } from './components'
+import { Footer, Navbar } from './components'
 
 function App() {
  
@@ -9,6 +9,7 @@ function App() {
   return (
  <>
      <Navbar/>
+     <Footer/>
  </>
 
     
---
+++ fiber-landing-page/src/components/Footer.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Footer=()=>{
+    return(
+        <div className='mx-auto'>
+<div className='grid grid-cols-5 gap-2 justify-items-center ml-10'>
+    <div>
+        <h1 className='text-2xl font-bold'>Fiber</h1>
+        <p>With Fiber, you can setup your own personal portfolio in minutes with dozens of premade, beautiful templates</p>
+        <p>Made with Love!</p>
+    </div>
+    <div>
+        <h1 className='text-2xl font-bold'>Sitemap</h1>
+        <ul>
+            <li>  <a>Homepage</a></li>
+            <li> <a>Pricing</a></li>
+            <li>
+        <a>Testimonials</a>
+    </li>
+    <li>  <a>Features</a></li>
+        </ul>
+      
+         
+    </div>
+    <div>
+        <h1 className='text-2xl font-bold'>Resources</h1>
+        <ul>
+            <li>  <a>Support</a></li>
+            <li> <a>Contact</a></li>
+            <li>
+        <a>FAQ</a>
+    </li> 
+        </ul>
+      
+    </div>
+    <div>
+        <h1 className='text-2xl font-bold'>Company</h1>
+        <ul>
+            <li>  <a>About</a></li>
+            <li> <a>Customers</a></li>
+            <li>
+        <a>Blog</a>
+    </li> 
+        </ul>
+    </div>
+    <div>
+        <h1 className='text-2xl font-bold'>Portfolios</h1>
+        <ul>
+            <li>  <a>Sarah Andrews</a></li>
+            <li> <a>Mathew Higgins</a></li>
+     
+        </ul>
+    </div>
+ </div>
+        </div>
+ 
+        )
+}
+
+export default Footer
