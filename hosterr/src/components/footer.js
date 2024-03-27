66211b6a0bf39c3dda1e5b8f69618cc3163e89f2
gouribhise <gouri.a.bhise@gmail.com>
2024-03-27 20:38:04 +0530

    added footer

--- hosterr/src/components/footer.js
+++ hosterr/src/components/footer.js
@@ -1,8 +1,18 @@
 const Footer =()=>{
     return (
-        <div>
-            Footer
-        </div>
+        <div className="bg-white footer columns-2 sm:py-10">
+              <div className="mx-autolg:px-12">
+            <div className="px-6 ">
+              
+           <p className="text-3xl"> facebook  &nbsp; instagram  &nbsp;twitter</p>
+            </div>
+            <div>
+               <p className="text-2xl"> Have any question? &nbsp; 
+               <a href="#">Talk to specialist</a></p>
+            </div>
+            
+   </div>
+   </div>
     )
 }
 
