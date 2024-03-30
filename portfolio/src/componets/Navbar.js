65783e949cbf6135c8eb73567d89e0d93780b268
gouribhise <gouri.a.bhise@gmail.com>
2024-03-30 15:57:52 +0530

    added navbar

--- portfolio/src/componets/Navbar.js
+++ portfolio/src/componets/Navbar.js
@@ -1,11 +1,34 @@
 const Navbar=()=>{
     return(
-        <div>
-            <a>Home</a>
-            <a>Skills</a>
-            <a>About</a>
-            <a>Projects</a>
-        </div>
+        <nav className='bg-emerald-100'>
+      <div className='align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
+      <h2 className='text-3xl font-bold'>
+          Web<span className='text-emerald-600'>Dev</span>
+        </h2>
+     <div className='flex gap-x-3 sm:columns-1'>
+                
+                    <a href="#" className='sm:columns-1 capitalize text-lg tracking-wide hover:text-emerald-600 duration-300'>
+                        Home
+                    </a>
+                 
+           
+                    <a href="#" className='sm:columns-1 capitalize text-lg tracking-wide hover:text-emerald-600 duration-300'>
+                        Skills
+                    </a>
+       
+         
+                    <a href="#" className='sm:columns-1 capitalize text-lg tracking-wide hover:text-emerald-600 duration-300'>
+                        About
+                    </a>
+        
+           
+                    <a href="#" className='sm:columns-1 capitalize text-lg tracking-wide hover:text-emerald-600 duration-300'>
+                        Projects
+                    </a>
+             </div>
+            </div>
+           
+        </nav>
     )
 }
 
